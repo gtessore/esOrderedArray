40c6a80b8421a5f663c52bf1fcef0c9a78ace88b
Gabriele Tessore <gabriele.tessore@edu.unito.it>
2022-03-22 11:28:03 +0100

    Update OrderedArray.java
    first commit, to try

--- src/OrderedArray.java
+++ src/OrderedArray.java
@@ -1,3 +1,15 @@
-public class OrderedArray {
+import java.util.ArrayList;
+import java.util.Comparator;            //classe che serve per fare il confronto
 
+/*
+* This class maintains the elements inserted according to a criterium specified at creation time
+* @param T : Type of elements in the array
+* */
+
+public class OrderedArray <T> {
+    ArrayList <T> array = null;
+    Comparator <? super T> comparator = null;           //si inizializza a null
+
+    public OrderedArray() {
+    }
 }
