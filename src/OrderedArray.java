d1ed7529e7f1c6dab6b52026c5ceb9dc0531ffc5
Gabriele Tessore <gabriele.tessore@edu.unito.it>
2022-03-22 11:57:58 +0100

    added isEmpty, size, get(index)

--- src/OrderedArray.java
+++ src/OrderedArray.java
@@ -7,13 +7,15 @@ import java.util.Comparator;            //classe che serve per fare il confronto
 * */
 
 public class OrderedArray <T> {
-    ArrayList <T> array = null;
-    Comparator <? super T> comparator = null;
+    private ArrayList <T> array = null;
+    private Comparator <? super T> comparator = null;
 
     /*
     Creates an empty Ordered Array
     takes a Comparator as input
-
+    @author gtessore
+    @param comparator :
+    @throws: OrderedArrayException
      */
     public OrderedArray(Comparator <? super T> comparator) throws OrderedArrayException {
         if(comparator == null) {
@@ -21,6 +23,29 @@ public class OrderedArray <T> {
         }
         this.array = new ArrayList<>();
         this.comparator = comparator;
+    }
+    /*
+    @return True iff this ordered array is empty
+     */
+    public boolean isEmpty() {
+        return this.array.isEmpty();
+    }
 
+    /*
+    @return the number of elements currently in this ordered array
+     */
+    public int size(){
+        return this.array.size();
+    }
+
+    /*
+    @return the element at specified index (starting from 0)
+    @throws OrderedArrayException
+     */
+    public T get(int index) throws OrderedArrayException {
+        if(index<0 || index>=this.size()){
+            throw new OrderedArrayException("OrderedArray.get: received index out of bounds (<0 or >size)");
+        }
+        return this.array.get(index);
     }
 }
